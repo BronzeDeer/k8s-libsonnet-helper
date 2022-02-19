7b6335c4eed3eb67025faa9ad055f21767a5a87d
Joel Pepper <pepper@bronze-deer.de>
2022-02-19 16:23:16 +0100

    Rename internal variables to minize interference with env

--- setup-k8s-libsonnet.sh
+++ setup-k8s-libsonnet.sh
@@ -1,7 +1,7 @@
 #! /bin/bash
 
-SCRIPT_DIR=$(realpath ${BASH_SOURCE[0]})
-SCRIPT_DIR=$(dirname $SCRIPT_DIR)
+__SCRIPT_DIR=$(realpath ${BASH_SOURCE[0]})
+__SCRIPT_DIR=$(dirname $__SCRIPT_DIR)
 
 if [ ! -f "jsonnetfile.json" ]; then
   jb init
@@ -9,23 +9,23 @@ fi
 
 if [ -z "${1+x}" ]; then
   # Automatically set to version of server pointed to by current kubeconfig
-  VERSION=$(kubectl version -o yaml | yq '[.serverVersion.major, .serverVersion.minor ] | join(".")')
+  __VERSION=$(kubectl version -o yaml | yq '[.serverVersion.major, .serverVersion.minor ] | join(".")')
 else
   if [[ $1 =~ v?([0-9]+\.[0-9]+)(\.[0-9]+)? ]]; then
-    VERSION=${BASH_REMATCH[1]}
+    __VERSION=${BASH_REMATCH[1]}
   else
     echo "Version string must match the regexp 'v?([0-9]+\.[0-9]+)(\.[0-9]+)?'"
     exit 1
   fi
 fi
 
-jb install "https://github.com/jsonnet-libs/k8s-libsonnet/${VERSION}@main"
+jb install "https://github.com/jsonnet-libs/k8s-libsonnet/${__VERSION}@main"
 
-echo "import \"./submodule/${VERSION}/main.libsonnet\"" > lib/k.libsonnet
+echo "import \"./submodule/${__VERSION}/main.libsonnet\"" > lib/k.libsonnet
 
-echo "Setup k.libsonnet for k8s version $VERSION. To use it, append $SCRIPT_DIR/lib to your jpath or source this script"
+echo "Setup k.libsonnet for k8s version $__VERSION. To use it, append $__SCRIPT_DIR/lib to your jpath or source this script"
 
 # Enable easy sourcing of this script
-JSONNET_PATH="$JSONNET_PATH:$SCRIPT_DIR/lib"
-unset SCRIPT_DIR
-unset VERSION
+JSONNET_PATH="$JSONNET_PATH:$__SCRIPT_DIR/lib"
+unset __SCRIPT_DIR
+unset __VERSION
